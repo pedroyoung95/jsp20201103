634d2f2a4c0d2d35e53441025b630e1a185648ce
pedroyoung95 <jty9531@gmail.com>
2020-11-10 12:36:53 +0900

    11/10 12:36 수업내용

--- myjsp/src/chap09/Cookies.java
+++ myjsp/src/chap09/Cookies.java
@@ -27,7 +27,7 @@ public class Cookies {
 		if(cookie == null) {
 			return null;
 		}
-		return URLDecoder.decode(cookie.getValue(), "euc-kr");
+		return URLDecoder.decode(cookie.getValue(), "utf-8");
 	}
 	
 	public boolean exists(String name) {
@@ -36,6 +36,13 @@ public class Cookies {
 	
 	public static Cookie createCookie(String name, String value) throws IOException {
 		return new Cookie(name, URLEncoder.encode(value, "utf-8"));
+	}
+	
+	public static Cookie createCookie(String name, String value, String path, int maxAge) throws IOException {
+		Cookie cookie = new Cookie(name, URLEncoder.encode(value, "utf-8"));
+		cookie.setPath(path);
+		cookie.setMaxAge(maxAge);
+		return cookie;
 	}
 	
 	public static Cookie createCookie(String name, String value, String domain, String path, int maxAge) throws IOException {
