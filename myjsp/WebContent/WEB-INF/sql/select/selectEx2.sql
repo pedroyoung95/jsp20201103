d1ce0f5137195d4f5734c091f955073c6ca3421f
pedroyoung95 <jty9531@gmail.com>
2020-11-12 12:47:33 +0900

    11/12 12:47 수업내용

--- myjsp/WebContent/WEB-INF/sql/select/selectEx2.sql
+++ myjsp/WebContent/WEB-INF/sql/select/selectEx2.sql
@@ -31,3 +31,56 @@ SELECT * FROM employee WHERE salary < 1000 OR salary >1500;
 SELECT * FROM employee WHERE commission = 300 OR commission = 500 OR commission = 1400;
 
 SELECT * FROM employee WHERE salary BETWEEN 1000 AND 1500;
+SELECT * FROM employee WHERE salary >= 1000 AND salary <= 1500;
+SELECT * FROM employee WHERE salary NOT BETWEEN 1000 AND 1500;
+SELECT * FROM employee WHERE salary < 1000 OR salary > 1500;
+SELECT * FROM employee WHERE hiredate BETWEEN '1982/01/01' AND '1982/12/31';
+
+SELECT * FROM employee WHERE commission IN(300, 500, 1400);
+SELECT * FROM employee WHERE commission = 300 OR commission = 500 OR commission = 1400;
+SELECT * FROM employee WHERE commission NOT IN (300, 500, 1400);
+SELECT * FROM employee WHERE commission != 300 AND commission != 500 AND commission != 1400;
+
+SELECT * FROM employee WHERE ename LIKE 'F%';
+SELECT * FROM employee WHERE ename LIKE '%M%';
+SELECT * FROM employee WHERE ename LIKE '%N';
+SELECT * FROM employee WHERE ename LIKE '_A%';
+SELECT * FROM employee WHERE ename LIKE '__A%';
+SELECT * FROM employee WHERE ename NOT LIKE '%A%';
+
+SELECT * FROM employee WHERE commission IS NULL;
+SELECT * FROM employee WHERE commission IS NOT NULL;
+
+SELECT * FROM employee ORDER BY salary ASC; --기본이 asc이므로 asc는 생략 가능
+SELECT * FROM employee ORDER BY salary DESC;
+SELECT * FROM employee ORDER BY ename;
+SELECT * FROM employee ORDER BY hiredate DESC;
+SELECT * FROM employee ORDER BY salary DESC, ename ASC;
+
+--혼자 해보기 p.65~
+SELECT ename, salary, salary+300 FROM employee;
+
+SELECT ename, salary, salary*12+100 FROM employee ORDER BY salary*12+100 DESC;
+SELECT ename, salary, salary*12+100 AS "연간 총수입" FROM employee ORDER BY "연간 총수입" DESC;
+
+SELECT ename, salary FROM employee WHERE salary > 2000 ORDER BY salary DESC;
+
+SELECT ename, dno FROM employee WHERE eno = 7788;
+
+SELECT ename, salary FROM employee WHERE salary NOT BETWEEN 2000 AND 3000;
+
+SELECT ename, job, hiredate FROM employee WHERE hiredate BETWEEN '1981/02/20' AND '1981/05/01';
+
+SELECT ename, dno FROM employee WHERE dno BETWEEN 20 AND 30 ORDER BY ename ASC;
+
+SELECT ename, salary, dno FROM employee WHERE salary BETWEEN 2000 AND 3000 AND dno BETWEEN 20 AND 30 ORDER BY ename;
+
+SELECT ename, hiredate FROM employee WHERE hiredate LIKE '81%'; --왜 1981%은 안 되는지?
+
+SELECT ename, JOB FROM employee WHERE MANAGER IS NULL;
+
+SELECT ename, salary, commission FROM employee WHERE commission IS NOT NULL ORDER BY salary, commission;
+
+SELECT ename FROM employee WHERE ename LIKE '__R%';
+
+select ename from employee where ename like '%A%' and ename like '%E%';
