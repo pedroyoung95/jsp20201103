5138558fa7f140e4d81ec0c9f4160879c7905d70
pedroyoung95 <jty9531@gmail.com>
2020-11-17 10:42:40 +0900

    11/17 10:42 수업내용

--- myjsp/WebContent/WEB-INF/sql/joinTable/joinEx1.sql
+++ myjsp/WebContent/WEB-INF/sql/joinTable/joinEx1.sql
@@ -31,7 +31,40 @@ SELECT ename, e.dno, dname
 FROM employee e, department d
 WHERE e.dno = d.dno AND ename = 'SCOTT';
 
+--2번
+SELECT ename, dname, loc FROM employee e JOIN department d ON e.dno = d.dno;
+
+--3번
+SELECT dno, job, loc FROM employee JOIN department USING(dno) WHERE dno = 10;
+
+--4번
+SELECT ename, dname, loc FROM employee NATURAL JOIN department WHERE commission IS NOT NULL;
+
 --5번
 SELECT ename, dname
 FROM employee e, department d
-WHERE e.dno = d.dno AND ename = '%A%';
+WHERE e.dno = d.dno AND ename like '%A%';
+
+--6번
+SELECT ename, job, dno, dname FROM employee NATURAL JOIN department WHERE loc = 'NEW YORK';
+
+--join keyword(INNER JOIN, JOIN이라는 키워드를 안에 작성하는 조인 방법)
+SELECT * FROM employee NATURAL JOIN department; --NATURAL JOIN 공통 칼럼을 조사한 후 자동으로 조인 수행(같은 칼럼명이 존재해야 함)
+SELECT * FROM employee e NATURAL JOIN department
+WHERE e.eno=7788; --공통 칼럼이 이름과 타입 둘 다 같아야 함
+
+--JOIN USING
+SELECT * FROM employee JOIN department USING(dno); --기준이 되는 공통칼럼을 USING으로 지정(공통 칼림이 이름만 같아도 됨)
+    --NATURAL JOIN과 결과가 동일
+SELECT e.eno, e.ename, d.dname, dno
+FROM employee e JOIN department d USING(dno)
+WHERE e.eno = 7788; --NATURAL JOIN, INNER JOIN, JOIN USING은 공통칼럼을 알아서 조사하므로, 모호성 신경 안 써도 됨
+
+--JOIN ON
+SELECT * FROM employee e JOIN department d ON e.dno = d.dno; --칼럼명, 칼럼타입 모두 달라도 JOIN기준을 설정 가능
+SELECT * FROM employee e JOIN department d ON e.dno = d.dno
+WHERE eno = 7788;
+SELECT e.eno, e.ename, d.dname, e.dno
+FROM employee e JOIN department d 
+ON e.dno = d.dno
+WHERE eno = 7788; --on은 키워드를 안 쓰는 join과 마찬가지로, 공통 칼럼의 모호성을 제거해줘야 함
