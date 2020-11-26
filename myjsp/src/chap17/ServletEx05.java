ea59c70c42ba8a92c38f891bd0e370e64054ed1e
pedroyoung95 <jty9531@gmail.com>
2020-11-26 11:36:05 +0900

    11/26 11:35 수업내용

---
+++ myjsp/src/chap17/ServletEx05.java
@@ -0,0 +1,50 @@
+package chap17;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletEx05
+ */
+@WebServlet("/ex05")
+public class ServletEx05 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletEx05() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		System.out.println(id);
+		System.out.println(name);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8"); //한글 파라미터 값을 읽기 위해 필요함
+		String age = request.getParameter("age");
+		String email = request.getParameter("email");
+		System.out.println(age);
+		System.out.println(email);
+		doGet(request, response);
+	}
+
+}
