10500d1800c9d5d2ec64b7058de3329f9226a145
pedroyoung95 <jty9531@gmail.com>
2020-11-30 10:41:32 +0900

    11/30 10:41 수업내용

---
+++ myjsp/src/chap18/complex1/Model1.java
@@ -0,0 +1,7 @@
+package chap18.complex1;
+
+public class Model1 {
+	public Object method1() {
+		return "안녕하세요";
+	}
+}
---
+++ myjsp/src/chap18/complex1/Model2.java
@@ -0,0 +1,9 @@
+package chap18.complex1;
+
+import java.util.Date;
+
+public class Model2 {
+	public Object domodel2() {
+		return new Date();
+	}
+}
