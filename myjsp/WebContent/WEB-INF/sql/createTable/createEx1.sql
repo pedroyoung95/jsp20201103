baeb52c19398bb5011f8c1594504229bbf738b78
pedroyoung95 <jty9531@gmail.com>
2020-12-01 18:03:22 +0900

    12/1 18:03 예습

--- myjsp/WebContent/WEB-INF/sql/createTable/createEx1.sql
+++ myjsp/WebContent/WEB-INF/sql/createTable/createEx1.sql
@@ -2,3 +2,28 @@ CREATE TABLE dept(
     dno NUMBER(2),
     dname VARCHAR2(14),
     loc VARCHAR2(13));
+    
+SELECT * FROM dept;
+    
+CREATE TABLE dept_second
+AS 
+SELECT *
+FROM department;
+
+SELECT * FROM dept_second;
+
+CREATE TABLE dept20
+AS
+SELECT eno, ename, salary *12 annsal
+FROM employee
+WHERE dno = 20;
+
+SELECT * FROM dept20;
+
+CREATE TABLE dept_third
+AS
+SELECT dno, dname
+FROM DEPT
+WHERE 0=1;
+
+SELECT * FROM dept_third;
