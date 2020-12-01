2aa301bea50ab3f2d15ed988ee0fcd6066344c17
pedroyoung95 <jty9531@gmail.com>
2020-12-01 16:02:24 +0900

    12/1 16:02 수업내용

--- myjsp/WebContent/WEB-INF/sql/subQuery/subEx1.sql
+++ myjsp/WebContent/WEB-INF/sql/subQuery/subEx1.sql
@@ -37,6 +37,17 @@ SELECT eno, ename FROM employee
 WHERE (dno, salary) IN (SELECT dno, MIN(salary) FROM employee
                     GROUP BY dno); --여러 컬럼을 조건으로 걸 수 있음 
                     
+SELECT eno, ename, job, salary
+FROM employee
+WHERE salary < ANY(SELECT salary FROM employee WHERE job = 'SALESMAN')
+AND job != 'SALESMAN'; --ANY는 임의의 값과의 비교이기 때문에, 최고 값보다 작거나 최소 값보다 크기만 하면 true
+--ANY의 결과는 SOME과 동일하기 때문에 SOME과 바꿔도 가능
+
+SELECT eno, ename, job, salary
+FROM employee
+WHERE salary < ALL (SELECT salary FROM employee WHERE job = 'SALESMAN')
+AND job ! ='SALESMAN';
+                    
 --혼자해보기
 
 --1번
@@ -74,4 +85,51 @@ SELECT eno, ename, job, salary FROM employee
 WHERE job != 'ANALYST' AND salary < ANY (SELECT salary FROM employee WHERE job = 'ANALYST');
 
 --7번 
-SELECT 
+SELECT ename FROM employee
+WHERE eno IN (SELECT eno FROM employee WHERE manager IS NULL);
+
+--8번
+SELECT ename FROM employee
+WHERE eno IN (SELECT eno FROM employee WHERE manager IS NOT NULL);
+
+--9번
+SELECT ename, hiredate FROM employee 
+WHERE dno = (SELECT dno FROM employee WHERE ename = 'BLAKE' ) and ename != 'BLAKE';
+
+--10번
+SELECT eno, ename FROM employee
+WHERE salary > (SELECT AVG(salary) FROM employee)
+ORDER BY salary;
+
+--11번
+SELECT eno, ename FROM employee
+WHERE dno in (SELECT dno FROM employee WHERE ename LIKE '%K%');
+
+--12번
+SELECT ename, dno, job FROM employee
+WHERE dno = (SELECT dno FROM department WHERE loc = 'DALLAS');
+
+--13번
+SELECT ename, salary FROM employee
+WHERE manager = (SELECT eno FROM employee WHERE ename='KING');
+
+--14번
+SELECT dno, ename, job FROM employee
+WHERE dno = (SELECT dno FROM department WHERE dname = 'RESEARCH');
+
+--15번
+SELECT eno, ename, salary FROM employee
+WHERE salary > (SELECT AVG(salary) FROM employee)
+AND dno IN (SELECT dno FROM employee WHERE ename LIKE '%M%');
+
+--16번
+SELECT job, AVG(salary) FROM employee
+GROUP BY job
+HAVING AVG(salary) = (SELECT MIN(AVG(salary)) FROM employee GROUP BY job);
+
+--17번
+SELECT ename FROM employee 
+WHERE eno in (SELECT manager FROM employee);
+
+SELECT * FROM employee;
+
