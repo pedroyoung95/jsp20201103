24729994f5fc24a71c655f70a010dcb301502aaa
pedroyoung95 <jty9531@gmail.com>
2020-12-16 11:30:10 +0900

    12/16 11:29 데이터테이블

--- myjsp/WebContent/WEB-INF/sql/sql/myboard.sql
+++ myjsp/WebContent/WEB-INF/sql/sql/myboard.sql
@@ -11,7 +11,7 @@ SELECT * FROM member;
 
 ---------------------------------
 
-DROP TABLE article;
+--DROP TABLE article;
 
 CREATE TABLE article(
     article_no NUMBER GENERATED AS IDENTITY,
@@ -24,11 +24,33 @@ CREATE TABLE article(
     PRIMARY KEY(article_no)
 );
 SELECT * FROM article;
+SELECT * FROM (
+SELECT article_no, title, ROW_NUMBER() OVER(ORDER BY article_no DESC) rn
+FROM article
+--ORDER BY article_no DESC
+) WHERE rn BETWEEN 1 AND 5;
 
-DROP TABLE article_content;
+--DROP TABLE article_content;
 
 CREATE TABLE article_content(
     article_no NUMBER PRIMARY KEY,
     content VARCHAR2(4000)
 );
 SELECT * FROM article_content;
+
+------------댓글 
+--DROP TABLE reply;
+
+CREATE TABLE reply(
+    replyid NUMBER GENERATED AS IDENTITY,
+    memberid VARCHAR2(50) NOT NULL,
+    article_no NUMBER NOT NULL,
+    body VARCHAR2(1000) NOT NULL,
+    regdate DATE NOT NULL,
+    PRIMARY KEY(replyid)
+);
+SELECT * FROM reply;
+SELECT replyid, memberid, article_no, body, regdate
+FROM reply
+WHERE article_no=26
+ORDER BY replyid DESC;
